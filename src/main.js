58e4c10d8bac360664e89c543c6e2f7acf496984
Dilshod <abdurazzakov.dilshodbk97@mail.ru>
2025-11-30 21:03:17 +0100

    completed

---
+++ src/main.js
@@ -0,0 +1,86 @@
+import "./style.css";
+
+const containerEl = document.querySelector("#container");
+const forwardBtn = document.getElementById("forward-btn");
+const backwardBtn = document.getElementById("backward-btn");
+const playBtn = document.getElementById("play-btn");
+const musicTitleEl = document.getElementById("music-title");
+const audioEl = document.getElementById("audio");
+const changeVolumeEl = document.getElementById("change-volume");
+const coverEl=document.getElementById("cover")
+
+let currentVolume = +changeVolumeEl.value / 100;
+audioEl.volume = currentVolume;
+const musics = [
+  "bahrom-nazarov-atirguldan-farqingiz-yuq",
+  "botir_qodirov_-_xoji_onam_(uzhits.net)",
+  "botir-qodirov-sevgisi-yolgonim_(uzhits.net)",
+  "doston-ergashev-kambagalga_(uzhits.net)",
+  "gaybulla-tursunov-janona_(uzhits.net)",
+  "gulinur-janima_(uzhits.net)",
+  "hosila-rahimova-jetama_(uzhits.net)",
+  "jasmin-sevganlari-bor_(uzhits.net)",
+  "munisa-rizayeva-aka-makasi_(uzhits.net)",
+  "munisa-rizayeva-jaloliddin-ahmadaliyev-yetmasmidi-yetmadimi_(uzhits.net)",
+  "ozoda-nursaidova-kok-jiguli_(uzhits.net)",
+  "sardor-mamadaliyev-dada_(uzhits.net)",
+];
+
+let currentMusic = 0;
+
+const changeMusic = (curMus) => {
+  musicTitleEl.textContent = musics[curMus];
+  audioEl.src = `./music/${musics[curMus]}.mp3`;
+  coverEl.src = `./cover/${musics[curMus]}.jpg`;
+};
+
+changeMusic(currentMusic);
+
+const playMusic = () => {
+  containerEl.classList.add("play");
+  playBtn.innerHTML = '<i class="fa-solid fa-pause"></i>';
+  audioEl.play();
+};
+
+const pauseMusic = () => {
+  containerEl.classList.remove("play");
+  playBtn.innerHTML = '<i class="fas fa-play"></i>';
+  audioEl.pause();
+};
+
+const play = () => {
+  const isPlaying = containerEl.classList.contains("play");
+
+  if (isPlaying) {
+    pauseMusic();
+  } else {
+    playMusic();
+  }
+};
+const nextSong = () => {
+  currentMusic++;
+  changeMusic(currentMusic);
+  playMusic();
+  if(currentMusic==musics.length-1){
+    currentMusic=0;
+  }
+};
+
+const prevSong = () => {
+  currentMusic--;
+  changeMusic(currentMusic);
+  playMusic();
+  if(currentMusic<0){
+    currentMusic=musics.length-1;
+  }
+};
+
+const changeVolume = () => {
+  currentVolume = +changeVolumeEl.value / 100;
+  audioEl.volume = currentVolume;
+};
+
+playBtn.addEventListener("click", play);
+forwardBtn.addEventListener("click", nextSong);
+backwardBtn.addEventListener("click", prevSong);
+changeVolumeEl.addEventListener("input", changeVolume);
